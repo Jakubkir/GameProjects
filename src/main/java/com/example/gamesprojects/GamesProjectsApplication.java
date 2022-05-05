f58f8ed393de18dbde3cf95f5bc82c5d59451ff0
Jakub Kirker <jakubkir@wp.pl>
2022-05-05 19:09:40 +0200

    Frist commit & Katalogi

---
+++ src/main/java/com/example/gamesprojects/GamesProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamesprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesProjectsApplication.class, args);
+    }
+
+}

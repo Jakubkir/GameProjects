a6d41567f2e76f68df7fc07cb8b10c17a6dffadd
Jakub Kirker <jakubkir@wp.pl>
2022-05-05 19:11:12 +0200

    CartController

---
+++ src/main/java/com/example/gamesprojects/controller/GamesController.java
@@ -0,0 +1,50 @@
+package com.example.gamesprojects.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/v1/games")
+@RequiredArgsConstructor
+public class GamesController {
+
+    private final GameService gameService;
+    private final GameMapper gameMapper;
+
+    @GetMapping
+    public ResponseEntity<List<GameDto>> getGames() {
+        List<Game> games = gameService.getAllGames();
+        return ResponseEntity.ok(gameMapper.mapToGameDtoList(games));
+    }
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<GameDto> getGame(@PathVariable Long id) throws GameNotFoundException {
+        return ResponseEntity.ok(gameMapper.mapToGameDto(gameDbService.getGames(gameId)));
+    }
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<Void> deleteGame(@PathVariable Long id) throws GameNotFoundException {
+        gameService.deleteGame(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<GameDto> updateGame(@@RequestBody GameDto gameDto) {
+        Game game = gameMapper.mapToGame(gameDto);
+        Game savedgame = gameService.saveGame(game);
+        return ResponseEntity.ok(gameMapper.mapToGameDto(savedgame));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createGame(@RequestBody GameDto gameDto) {
+        Game game = gameMapper.mapToGame(gameDto);
+        gameService.saveGame(game);
+        return ResponseEntity.ok().build();
+    }
+}
+}

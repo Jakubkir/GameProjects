60eccad62597a3b3c1121fa6f73f713382b16950
Jakub Kirker <jakubkir@wp.pl>
2022-05-05 19:17:17 +0200

    GameRespository respository

---
+++ src/main/java/com/example/gamesprojects/respository/GameRespository.java
@@ -0,0 +1,23 @@
+package com.example.gamesprojects.respository;
+
+import com.example.gamesprojects.domain.Game;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public interface GameRespository extends CrudRepository<Game, Long> {
+
+    @Override
+    List<Game> findAll();
+
+    @Override
+    Game save(Game game);
+
+    @Override
+    Optional<Game> findById(Long id);
+}
